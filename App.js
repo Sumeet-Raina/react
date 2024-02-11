79825478f4cb2da4e5ff5a36b36b3752e8962bd7
Sumeet <sraina@worldremit.com>
2024-02-11 20:27:45 +0000

    Implemented a new functional component named HeadingComponent, showcasing the use of JSX for defining and integrating a static title element. This component demonstrates the foundational approach to constructing React components with embedded JSX elements, focusing on accessibility with tabIndex and class-based styling.

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "React");
+// title is a react element
+const title = (
+  <h1 className="head" tabIndex="5">
+    react using JSX
+  </h1>
+);
+//React functional component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      {title}
+      <h1>React functional component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<HeadingComponent />);
