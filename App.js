dba6ed7ae01e8ddf6f616b804a74b0e50c05144f
Sumeet <sraina@worldremit.com>
2024-02-24 20:46:17 +0000

    Added root component. Added header and body components. Added css for header and restaurant card.

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
+import "./index.css";
 
-// title is a react component
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    react using JSX
-  </h1>
-);
-//React functional component
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      {Title()}
-      <Title />
-      <Title></Title>
-      <h1>React functional component</h1>
+    <div className="header">
+      <div classname="logo-container">
+        <img src={logo} className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghna foods</h3>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div class="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+
+root.render(<AppLayout />);
